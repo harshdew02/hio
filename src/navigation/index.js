d93f6e0b07b03e2ca4d85273c9367508d7b74c73
D Anuj Kumar <anuj.as828@gmail.com>
2024-01-05 16:27:45 +0530

    api response time optimized

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -11,16 +11,32 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import RightDrawer from "./RightDrawer";
 import BottomTabs from "./BottomTabs";
 import Verify from "../screens/Verify";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 export default function AppNavigation() {
 
+    
     const Stack = createNativeStackNavigator();
+    const [front, setFront] = React.useState('LoginPage')
 
+    // React.useEffect(() => {
+    //     const byPass = async () => {
+    //         let token = await AsyncStorage.getItem("token", (data) => {
+    //             console.log(data);
+    //           });
+    //           if (token == null || token == undefined) {
+    //             // setFront('main')
+    //           } else setFront('main')
+    //     }
+    //     byPass();
+    //     // console.log(token)
+    // }, [])
+    
     return (
         <NavigationContainer>
             <Stack.Navigator
-                initialRouteName="LoginPage"
+                initialRouteName={`${front}`}
                 screenOptions={{ headerShown: false }}
             >
                 <Stack.Screen name="verifyPage" component={Verify} />
--- src/screens/Verify.js
+++ src/screens/Verify.js
@@ -1,44 +1,70 @@
 import {
-    View,
-    Text,
-    SafeAreaView,
-    Image,
-    StatusBar,
-    StyleSheet,
-    TextInput,
-    TouchableOpacity,
-    ScrollView,
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  StatusBar,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import React, { useState, useEffect } from "react";
 import TopBar from "../components/TopBar";
 import {
-    widthPercentageToDP as wp,
-    heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import { Dropdown } from "react-native-element-dropdown";
 import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+import { useNavigation } from "@react-navigation/native";
 
+const verifyOTP = (mobile, Token, otp, navigation) => {
+  const apiUrl = "https://heartitout.in/welcome/wp-json/check_details/v1";
 
-import { useNavigation } from '@react-navigation/native';
+  try {
+    const requestData = {
+      mob: mobile,
+      token: Token,
+      otp: otp,
+    };
 
+    axios.post(apiUrl, requestData).then((res) => {
+      if (res.data.Status == "Get_Details") {
+        navigation.navigate("main");
+        // AsyncStorage.setItem("token", Token).then((value) => {
+        //   console.log(value);
+        // });
+        //Tokenization here
+      } else if (res.data.Status == "Success") {
+        // AsyncStorage.setItem("token", Token);
+        navigation.navigate("main");
+        //Tokenization here
+      } else {
+        console.log("wrong otp received");
+      }
+    }).catch((err)=>{
+      console.log(err)
+    });
+  } catch (error) {
+    console.error("Error requesting OTP:", error.message);
+  }
+};
 
+export default function Verify({ navigation, route }) {
+  const [value, setValue] = useState("91");
 
-export default function Verify() {
+  // const navigation = useNavigation();
 
-    const [value, setValue] = useState('91');
+  const [number, onChangeNumber] = React.useState("");
 
-    const navigation = useNavigation();
-
-    const [number, onChangeNumber] = React.useState("");
-
-
-    return (
-        <SafeAreaView>
+  return (
+    <SafeAreaView>
       <TopBar />
       <ScrollView>
-
         <View style={styles.box}>
           <View className="bg-[#EAF7FC]" style={styles.vect}></View>
           <View className="flex-col justify-end" style={{ height: hp(45) }}>
@@ -69,15 +95,12 @@ export default function Verify() {
             <Text className="text-base font-bold">All in one place!</Text>
           </View> */}
 
-          <Text className="text-[22px] font-[700]">
-            Enter Your OTP
-          </Text>
+          <Text className="text-[22px] font-[700]">Enter Your OTP</Text>
 
           <View
             className="flex-row justify-around items-center mt-8"
             style={{ width: wp(85) }}
           >
-
             <TextInput
               className="rounded-lg"
               style={styles.input}
@@ -91,86 +114,90 @@ export default function Verify() {
           <TouchableOpacity
             className="bg-[#32959D] rounded-full py-3 mt-8 items-center"
             style={{ height: hp(7), width: wp(80) }}
-            // onPress={async () => {
-            //   await requestOTP(value,number);
-            // }}
-            onPress={()=>{navigation.navigate('main')} }
+            onPress={() => {
+              // await verifyOTP("91","9399435543",);
+              verifyOTP(
+                route.params.mobile,
+                route.params.Token,
+                number,
+                navigation
+              );
+            }}
+            // onPress={()="">{} }
           >
             <Text className="text-[22.48px] text-white">Verify OTP</Text>
           </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
-    )
+  );
 }
 
-
 const styles = StyleSheet.create({
-    vect: {
-      width: "150%",
-      height: "95%",
-      background: "#EAF7FC",
-      borderBottomRightRadius: 350,
-      borderBottomLeftRadius: 350,
-      position: "absolute",
-    },
-    box: {
-      // overflow: 'hidden',
-      alignItems: "center",
-      height: hp(40),
-      width: wp(100),
-      // backgroundColor: 'red'
-    },
-    display: {
-      width: wp(60),
-      height: wp(61),
-    },
-    input: {
-      height: hp(7),
-      width: wp(70),
-      borderWidth: 1,
-      padding: 10,
-    },
-    dropdown: {
-      marginTop: 7,
-      height: hp(7),
-      width: wp(20),
-      backgroundColor: "white",
-      borderRadius: 12,
-      borderColor: "black",
-      borderWidth: 1,
-      borderStyle: "solid",
-      padding: 10,
-      left: 0,
-      zIndex: 3,
-      position: "absolute",
-    },
-    icon: {
-      marginRight: 5,
-    },
-    item: {
-      padding: 10,
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-    },
-    textItem: {
-      flex: 1,
-      fontSize: 16,
-    },
-    placeholderStyle: {
-      fontSize: 16,
-    },
-    selectedTextStyle: {
-      fontSize: 16,
-    },
-    iconStyle: {
-      width: 20,
-      height: 20,
-    },
-    inputSearchStyle: {
-      height: 40,
-      fontSize: 16,
-    },
-  });
-  
+  vect: {
+    width: "150%",
+    height: "95%",
+    background: "#EAF7FC",
+    borderBottomRightRadius: 350,
+    borderBottomLeftRadius: 350,
+    position: "absolute",
+  },
+  box: {
+    // overflow: 'hidden',
+    alignItems: "center",
+    height: hp(40),
+    width: wp(100),
+    // backgroundColor: 'red'
+  },
+  display: {
+    width: wp(60),
+    height: wp(61),
+  },
+  input: {
+    height: hp(7),
+    width: wp(70),
+    borderWidth: 1,
+    padding: 10,
+  },
+  dropdown: {
+    marginTop: 7,
+    height: hp(7),
+    width: wp(20),
+    backgroundColor: "white",
+    borderRadius: 12,
+    borderColor: "black",
+    borderWidth: 1,
+    borderStyle: "solid",
+    padding: 10,
+    left: 0,
+    zIndex: 3,
+    position: "absolute",
+  },
+  icon: {
+    marginRight: 5,
+  },
+  item: {
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textItem: {
+    flex: 1,
+    fontSize: 16,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
